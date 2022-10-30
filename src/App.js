33481b2db940f5453b787d66646355fde975a450
hari1538radha <hari.r2020ece@sece.ac.in>
2022-10-30 14:03:40 +0530

    setted up main container component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './component/Navbar/Navbar';
+import IntroContainer from './component/IntroContainer/IntroContainer';
 function App() {
   return (
     <div className="">
       
       <Navbar/>
+      <IntroContainer/>
     </div>
   );
 }
---
+++ src/component/IntroContainer/IntroContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IntroContainer = () => {
+  return (
+    <div className='h-[900px] bg-amber-400'>IntroContainer</div>
+  )
+}
+
+export default IntroContainer
